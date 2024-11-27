fe4405a55b48fdfbb0af4e7fa7bd2ae5a002d193
Endriw Braga <endriwbragalisboa@gmail.com>
2024-11-27 18:21:06 -0300

    Uso de sobrecarga no exericio, criação de mais 2 construtor, um vazio e outro com o mesmo nome do primeiro.

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -11,6 +11,8 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		Product p = new Product();
+		
 		System.out.println("Enter product data: ");
 		
 		System.out.print("Name: ");
@@ -19,10 +21,9 @@ public class Program {
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
 		
-		System.out.print("Quantity in stock: ");
-		int quantity = sc.nextInt();
+
 		
-		Product product = new Product(name, price, quantity);
+		Product product = new Product(name, price);
 		
 		System.out.println();
 		System.out.print("Product data: " + product);
@@ -31,7 +32,7 @@ public class Program {
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
 		
-		quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		product.addProducts(quantity);
 		
 		System.out.println();
