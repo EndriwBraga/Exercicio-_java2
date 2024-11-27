6afedb31b4bec1023521feea71f21ec120f6deb5
Endriw Braga <endriwbragalisboa@gmail.com>
2024-11-27 19:53:03 -0300

    Mudança dos atributos para privado, metodos para atualizar o nome e o preço. Inicio dos estudos em encapsulamento com java.

--- course/src/Entities/Product.java
+++ course/src/Entities/Product.java
@@ -2,40 +2,57 @@ package Entities;
 
 public class Product {
 
-	public String name;
-	public double price;
-	public int quantity;
-	
-	public Product() {}
-	
+	private String name;
+	private double price;
+	private int quantity;
+
+	public Product() {
+	}
+
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
-	} 
-	
+	}
+
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
-	} 
-	
-	public double ttlValueInStock()  {
-		return price * quantity;
-		
 	}
-	
+
+	public double ttlValueInStock() {
+		return price * quantity;
+
+	}
+
 	public void addProducts(int quantity) {
 		this.quantity += quantity;
 	}
-	
-	public void removeProducts (int quantity) {
+
+	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
 	public String toString() {
-		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, total: $ " 
-			+ String.format("%.2f", ttlValueInStock()) ;
-		
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, total: $ "
+				+ String.format("%.2f", ttlValueInStock());
+
 	}
 
 }
